39da0be7e3effa4fc49acbef853288ba29416531
Mustapha <adelajamustapha1@gmail.com>
2022-05-14 16:32:23 +0100

    add home screen

--- repair_home/lib/src/screens/home/home_screen.dart
+++ repair_home/lib/src/screens/home/home_screen.dart
@@ -29,114 +29,236 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Container(
-        padding: const EdgeInsets.all(4.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'What\'s broken?',
-                    style:
-                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  TextField(
-                    decoration: inputTextDecoration.copyWith(
-                        hintText: 'Search appliances',
-                        suffixIcon: Icon(Icons.screen_search_desktop_outlined,
-                            color: Colors.grey.shade500)),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  const Text(
-                    'Offers',
-                    style:
-                        TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(4.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'What\'s broken?',
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    TextField(
+                      decoration: inputTextDecoration.copyWith(
+                          hintText: 'Search appliances',
+                          suffixIcon: Icon(Icons.screen_search_desktop_outlined,
+                              color: Colors.grey.shade500)),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    const Text(
+                      'Offers',
+                      style: TextStyle(
+                          fontSize: 14.0, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Image.asset('assets/idek_man.png'),
-                  Image.asset('assets/idek_man2.png'),
-                ],
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Image.asset('assets/idek_man.png'),
+                    Image.asset('assets/idek_man2.png'),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  const Text(
-                    'We can fix it',
-                    style:
-                        TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4.0, horizontal: 16.0),
-                        decoration: BoxDecoration(
-                          color: bBlack,
-                          borderRadius: BorderRadius.circular(20.0),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    const Text(
+                      'We can fix it',
+                      style: TextStyle(
+                          fontSize: 14.0, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 4.0, horizontal: 16.0),
+                          decoration: BoxDecoration(
+                            color: bBlack,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: const Text(
+                            'Offers',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
-                        child: const Text(
-                          'Offers',
+                        const Text(
+                          'Kitchen',
                           style: TextStyle(
-                              color: Colors.white,
+                              color: textColor2,
                               fontSize: 14.0,
-                              fontWeight: FontWeight.bold),
+                              fontWeight: FontWeight.w500),
                         ),
+                        const Text(
+                          'Livingroom',
+                          style: TextStyle(
+                              color: textColor2,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        const Text(
+                          'Bathroom',
+                          style: TextStyle(
+                              color: textColor2,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: priColor3,
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      const Text(
-                        'Kitchen',
-                        style: TextStyle(
-                            color: textColor2,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.w500),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 4.0),
+                                decoration: BoxDecoration(
+                                  color: priColor2,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                child: const Icon(
+                                  Icons.microwave,
+                                  color: Colors.white,
+                                )),
+                          ),
+                          Expanded(
+                            flex: 8,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  20.0, 0.0, 0.0, 0.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Fix Microwave',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 12.0),
+                                  ),
+                                  Text(
+                                    'Kitchen',
+                                    style: TextStyle(
+                                        color: textColor2, fontSize: 12.0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const Expanded(
+                            flex: 1,
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 18.0,
+                            ),
+                          ),
+                        ],
                       ),
-                      const Text(
-                        'Livingroom',
-                        style: TextStyle(
-                            color: textColor2,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: priColor3,
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      const Text(
-                        'Bathroom',
-                        style: TextStyle(
-                            color: textColor2,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.w500),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 4.0),
+                                decoration: BoxDecoration(
+                                  color: priColor2,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                child: const Icon(
+                                  Icons.tv,
+                                  color: Colors.white,
+                                )),
+                          ),
+                          Expanded(
+                            flex: 8,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  20.0, 0.0, 0.0, 0.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Fix TV set',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 12.0),
+                                  ),
+                                  Text(
+                                    'Living room',
+                                    style: TextStyle(
+                                        color: textColor2, fontSize: 12.0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const Expanded(
+                            flex: 1,
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 18.0,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
