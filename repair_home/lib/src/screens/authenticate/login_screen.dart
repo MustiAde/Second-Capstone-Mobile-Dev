8bf552217626432b832ee6ae9daca2b31e76197a
Mustapha <adelajamustapha1@gmail.com>
2022-05-14 21:31:08 +0100

    add scrollable widget

--- repair_home/lib/src/screens/authenticate/login_screen.dart
+++ repair_home/lib/src/screens/authenticate/login_screen.dart
@@ -16,166 +16,178 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightWhite,
-      body: Container(
-          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
-          child: Stack(
-            children: [
-              // Image.asset('assets/blue_logo.png'),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: ((context) => const LoadingScreen()),
-                          ),
-                        );
-                      },
-                      child: Image.asset('assets/white_logo.png')),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: const Text(
-                      'Login to your account',
-                      style: TextStyle(
-                          fontSize: 16.0, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    decoration: customBoxDecor,
-                    child: TextFormField(
-                      decoration:
-                          inputTextDecoration.copyWith(hintText: 'Email'),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    decoration: customBoxDecor,
-                    child: TextFormField(
-                        decoration:
-                            inputTextDecoration.copyWith(hintText: 'Password'),
-                        obscureText: true),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: ((context) => const SignupPage()),
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      'Sign in',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(blueBlue),
-                      elevation: MaterialStateProperty.all(5.0),
-                      shadowColor: MaterialStateProperty.all(blueBlue),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Text(
-                    '• Or sign in with -',
-                    style: TextStyle(
-                      color: textColor2,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 16.0),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: InkWell(
-                          onTap: () {
-                            print('google');
-                          },
-                          child: Image.asset('assets/google.png'),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 16.0),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: InkWell(
-                          onTap: () {
-                            print('facebook');
-                          },
-                          child: Image.asset('assets/facebook.png'),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 16.0),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: InkWell(
-                          onTap: () {
-                            print('twitter');
-                          },
-                          child: Image.asset('assets/twitter.png'),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        '• Don\'t have an account? ',
-                        style: TextStyle(
-                          color: textColor2,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      InkWell(
+      body: SingleChildScrollView(
+        child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
+            child: Stack(
+              children: [
+                // Image.asset('assets/blue_logo.png'),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
                         onTap: () {
-                          print('sign up');
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: ((context) => const LoadingScreen()),
+                            ),
+                          );
                         },
-                        hoverColor: Colors.transparent,
-                        child: const Text(
-                          'Sign up',
+                        child: Image.asset('assets/white_logo.png')),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'Login to your account',
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Container(
+                      decoration: customBoxDecor,
+                      child: TextFormField(
+                        decoration:
+                            inputTextDecoration.copyWith(hintText: 'Email'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      decoration: customBoxDecor,
+                      child: TextFormField(
+                          decoration:
+                              inputTextDecoration.copyWith(hintText: 'Password'),
+                          obscureText: true),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: SizedBox(
+                            height: 50.0,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: ((context) => const SignupPage()),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Sign in',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(blueBlue),
+                                elevation: MaterialStateProperty.all(7.0),
+                                shadowColor: MaterialStateProperty.all(blueBlue),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 50.0,
+                    ),
+                    const Text(
+                      '• Or sign in with -',
+                      style: TextStyle(
+                        color: textColor2,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 16.0),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: InkWell(
+                            onTap: () {
+                              print('google');
+                            },
+                            child: Image.asset('assets/google.png'),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 16.0),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: InkWell(
+                            onTap: () {
+                              print('facebook');
+                            },
+                            child: Image.asset('assets/facebook.png'),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 16.0),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: InkWell(
+                            onTap: () {
+                              print('twitter');
+                            },
+                            child: Image.asset('assets/twitter.png'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 60.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          '• Don\'t have an account? ',
                           style: TextStyle(
-                            color: priColor2,
+                            color: textColor2,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          )),
+                        InkWell(
+                          onTap: () {
+                            print('sign up');
+                          },
+                          hoverColor: Colors.transparent,
+                          child: const Text(
+                            'Sign up',
+                            style: TextStyle(
+                              color: priColor2,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
