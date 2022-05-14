c3c1430e910ba9e0fce7413d31c275f4567f4f5d
Mustapha <adelajamustapha1@gmail.com>
2022-05-14 21:31:48 +0100

    add page route

--- repair_home/lib/src/screens/home/home_screen.dart
+++ repair_home/lib/src/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:repair_home/src/screens/home/fix_microwave.dart';
 import 'package:repair_home/src/shared/constants.dart';
 import 'package:repair_home/src/shared/widgets.dart';
 
@@ -137,61 +138,70 @@ class _HomeScreenState extends State<HomeScreen> {
                     const SizedBox(
                       height: 10.0,
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        color: priColor3,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 4.0),
-                                decoration: BoxDecoration(
-                                  color: priColor2,
-                                  borderRadius: BorderRadius.circular(10.0),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Microwave(),
+                            ));
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: priColor3,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 4.0),
+                                  decoration: BoxDecoration(
+                                    color: priColor2,
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: const Icon(
+                                    Icons.microwave,
+                                    color: Colors.white,
+                                  )),
+                            ),
+                            Expanded(
+                              flex: 8,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    20.0, 0.0, 0.0, 0.0),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      'Fix Microwave',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12.0),
+                                    ),
+                                    Text(
+                                      'Kitchen',
+                                      style: TextStyle(
+                                          color: textColor2, fontSize: 12.0),
+                                    ),
+                                  ],
                                 ),
-                                child: const Icon(
-                                  Icons.microwave,
-                                  color: Colors.white,
-                                )),
-                          ),
-                          Expanded(
-                            flex: 8,
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  20.0, 0.0, 0.0, 0.0),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: const [
-                                  Text(
-                                    'Fix Microwave',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 12.0),
-                                  ),
-                                  Text(
-                                    'Kitchen',
-                                    style: TextStyle(
-                                        color: textColor2, fontSize: 12.0),
-                                  ),
-                                ],
                               ),
                             ),
-                          ),
-                          const Expanded(
-                            flex: 1,
-                            child: Icon(
-                              Icons.arrow_forward_ios,
-                              size: 18.0,
+                            const Expanded(
+                              flex: 1,
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                size: 18.0,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                     const SizedBox(
